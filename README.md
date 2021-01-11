8b6770bd2b05cf0b0c4b7d7f35680c37024b713c
Louis Amaudruz <louis.amaudruz@epfl.ch>
2021-01-11 14:15:47 +0100

    Danger evaluation cleaned and results

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## Performance
 
+
 ### Training & Evaluation
 
 * Behavioral Cloning (Vanilla)
@@ -25,10 +26,41 @@
 
 <img src="crowd_nav/images/collision.png" height="240"/> <img src="crowd_nav/images/reward.png" height="240"/> 
  
+
+## Danger Evaluation
+
+The policies learned via imitation or reinforcement learning, even though highly performant in the environment they are trained in, suffer from the distributional shift
+in human behaviour during test time. Interacting with these new enviormnents might result in a high collision rate. The natural task for such scenarios would be to 
+have a mechanism to infer the robot-pedestrian collision probability of a new enviorments with the human motions as input. 
+
+The proposed imitator (TrajPred) human trajectory forecasting accuracy does not require interacting with the environment and can therefore be used to that end.
+
+### Evaluation 
+The imitator trained in the performance section is used to evaluate the method 
+
+In case you did not run the training in the first section of the readme, you can directly download the trained imitatior :
+ ```
+ TODO
+ ```
+ 
+ To test the method, we evaluate the correlation between trajectory forecasting accuracy and collision rate.
+ We sample multiple new environments and, for each :
+ 1. Have the robot observe human motions and test its trajectory forecasting accuracy
+ 2. Deploy the robot in this new simulated environment and observe the pedestrian-robot collision probability 
+ 
+Testing :
+ ```
+ python multi_env/correlation.py --figs 
+ ```
+ 
+ The testing is heavily time consuming (up to 12hrs), you can directly download the results via this command if desired :
+ ```
+ TODO
+ ```
+ 
+ <img src="crowd_nav/images/traj_coll_corr.png" height="500"/>
+ 
 ### Acknowledgments
 
 This code is developed based on [CrowdNav](https://github.com/vita-epfl/CrowdNav) and [Social-NCE](https://github.com/vita-epfl/social-nce)
-
-
-  
 
--- crowd_nav/multi_env/correlation.py
+++ crowd_nav/multi_env/correlation.py
@@ -1,26 +1,94 @@
+from configparser import Error
 import torch 
 import numpy as np
-
+import argparse
+import os
 import random
+import pandas as pd
+import seaborn as sns
+import shutil
 
-from crowd_nav.imitate import test_model
+from crowd_nav.utils.tests import test_model
+
+def pick_best_model(path) :
+    results = torch.load(os.path.join(path, 'results.pth'))
+    epoch, default_perf = max(results, key=lambda x : x[1]['reward'])
+    model_path = os.path.join(path, 'policy_net_{:02d}.pth'.format(epoch))
+    traj_path = os.path.join(path, 'prediction_head_{:02d}.pth'.format(epoch))
+    return model_path, traj_path, default_perf
+
+def plot_figs(df, output_dir) :
+    figs_path = os.path.join(output_dir, 'figs')
+    os.mkdir(figs_path)
+    sns.set_style("darkgrid")
+
+    plot = sns.scatterplot(
+    data=df,
+    x="trajectory_loss", y="collision"
+    )
+    plot.set(xlabel='Trajectory Loss', ylabel='Collision (%)')
+    fig = plot.get_figure()
+    fig.set_size_inches(15, 10)
+    fig.savefig(os.path.join(figs_path, 'traj_coll_corr.png'), dpi=300, bbox_inches='tight', pad_inches=0.1)
+
+    sns.set(font_scale=2)
+    plot = sns.heatmap(df.corr())
+    fig = plot.get_figure()
+    fig.set_size_inches(15, 10)
+    fig.savefig(os.path.join(figs_path, 'corr.png'), dpi=300, bbox_inches='tight', pad_inches=0.1)
 
 if __name__ == "__main__":
-    model_path = 'data/full_data_tests/trajpred/imitate-trajpred-2.50-weight-1to4-length-traj/policy_net_129.pth'
-    traj_path = 'data/full_data_tests/trajpred/imitate-trajpred-2.50-weight-1to4-length-traj/prediction_head_129.pth'
+    parser = argparse.ArgumentParser('Parse configuration file')
+    parser.add_argument('--policy', type=str, default='sail_traj')
+    parser.add_argument('--output_dir', type=str, default='data/multi_env')
+    parser.add_argument('--model_path', type=str, default='data/output/imitate-trajpred-2.50-weight-1to4-length-traj')
+    parser.add_argument('--figs', default=False, action='store_true')
+    
+    parser.add_argument('--n_envs', type=int, default=3000)
+    parser.add_argument('--save_interval', type=int, default=200)
 
-    test_grid_orca = {'safety_space' : np.linspace(0.01, 1, 100), 'neighbor_dist' : np.linspace(0, 6, 30), 
-                'time_horizon' : np.linspace(0, 10, 10)}
+    args = parser.parse_args()
 
-    n_tests=2000
-    results = []
+    if not os.path.exists(args.output_dir) :
+        os.mkdir(args.output_dir)
+    output_dir = os.path.join(args.output_dir, args.model_path.split('/')[-1])
+    if os.path.exists(output_dir) :
+        key = input('Path already exists, Overwrite ? (y/n) : ')
+        if key == 'n' :
+            raise Error
+        else :
+            shutil.rmtree(output_dir)
+    os.mkdir(output_dir)
+
+    model_path, traj_path, default_perf = pick_best_model(args.model_path)
+
+    n_tests=args.n_envs
+    results = {'perfs' : [], 'model_path' : model_path, 'traj_path' : traj_path, 'default_perf' : default_perf}
     for i in  range(n_tests):
-        if i % 100 == 0 :
-            torch.save(results, 'data/outlier/results.pth')
-        env_mods = {'safety_space' : random.choice(np.linspace(0.01, 0.8, 100)), 
-                    'neighbor_dist': random.choice(np.linspace(0, 6, 30)),
-                    'time_horizon' : random.choice(np.linspace(0, 10, 10))}
+        if i % args.save_interval == 0 :
+            torch.save(results, os.path.join(output_dir, 'results.pth'))
+        env_mods = {'safety_space' : random.choice(np.linspace(0.01, 1, 100)), 
+                    'neighbor_dist': random.choice(np.linspace(3, 10, 100))}
         
-        results.append((env_mods, test_model(m_p=model_path, model_type='sail_traj_simple', visible=False, n_episodes=100,\
+        results['perfs'].append((env_mods, test_model(m_p=model_path, model_type=args.policy, visible=False, n_episodes=100,\
             env_type='orca', traj_path=traj_path, env_mods=env_mods, notebook=False, suffix='{} :'.format(i))))
+    torch.save(results, os.path.join(output_dir, 'results.pth'))
+    
+    results = results['perfs']
+    traj_accuracy = [r[1]['traj accuracy'] for r in results]
+    collisions = np.asarray([1 - r[1]['success'] for r in results]) * 100
+    env_m = {k : [] for k in results[0][0].keys()}
+    for res in results :
+        for k in env_m.keys() :
+            env_m[k].append(res[0][k])
+    env_m['collision'] = collisions
+    env_m['trajectory_loss'] = traj_accuracy
+    df = pd.DataFrame(env_m)
+    torch.save(df, os.path.join(output_dir, 'results.csv'))
+
+    if args.figs :
+        plot_figs(df, output_dir)
+        
+
+
     
